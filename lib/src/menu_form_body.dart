e70a3a567b1b50a771db819aeffa5db0a05d16a0
gamingtechedu <gamingtechedu@gmail.com>
2024-11-13 09:18:01 -0300

    alter

--- lib/src/menu_form_body.dart
+++ lib/src/menu_form_body.dart
@@ -11,12 +11,15 @@ class MenuFormBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return Container(
+      padding: data.padding,
+      width: MediaQuery.of(context).size.width,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           if (data.inputs != null)
             SizedBox(
-              height: 300, // Defina uma altura apropriada
+              height: 300,
               child: ListView.builder(
                 controller: ScrollController(),
                 itemCount: data.inputs!.length,
@@ -28,34 +31,9 @@ class MenuFormBody extends StatelessWidget {
                   return null;
                 },
               ),
-            ),
+            )
         ],
       ),
     );
   }
 }
-
-
-//  Container(
-//       padding: data.padding,
-//       width: MediaQuery.of(context).size.width,
-//       child: Column(
-//         crossAxisAlignment: CrossAxisAlignment.stretch,
-//         children: [
-//           if (data.inputs != null)
-//             Expanded(
-//               child: ListView.builder(
-//                 controller: ScrollController(),
-//                 itemCount: data.inputs!.length,
-//                 itemBuilder: (context, index) {
-//                   final MenuFormItemData button = data.inputs![index];
-//                   if (button is CommonInputData) {
-//                     return CommonInput(data: button);
-//                   }
-//                   return null;
-//                 },
-//               ),
-//             )
-//         ],
-//       ),
-//     );
